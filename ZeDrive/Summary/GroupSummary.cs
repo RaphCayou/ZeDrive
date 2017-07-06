951612aa5933d4c633c17b45eb33f3211dab471f
jeepee12 <jeepee.12@hotmail.com>
2017-07-06 14:15:00 -0400

    Added the Summary project with the GroupSummary class.

---
+++ ZeDrive/Summary/GroupSummary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace Summary
+{
+    public class GroupSummary
+    {
+        public string GroupName;
+        public List<File> FileList;
+
+        public GroupSummary(string folderPath)
+        {
+            
+        }
+    }
+}
