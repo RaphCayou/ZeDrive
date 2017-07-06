2b4d6fc344363f07a0823b0eb7b875ba05ec2cce
Wingjam <jo_martineau_7@hotmail.com>
2017-07-06 11:28:53 -0400

    Added Summary Model

---
+++ ZeDrive/Models/Summary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Summary
+    {
+        public List<File> Files;
+        public Group Group;
+    }
+}
