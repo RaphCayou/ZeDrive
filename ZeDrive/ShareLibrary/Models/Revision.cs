a349508089cfbae1e4eb65800784bfbcf42347b3
jeepee12 <jeepee.12@hotmail.com>
2017-07-14 10:44:56 -0400

    Add Apply to Revision.

--- ZeDrive/ShareLibrary/Models/Revision.cs
+++ ZeDrive/ShareLibrary/Models/Revision.cs
@@ -1,4 +1,7 @@
-﻿namespace ShareLibrary.Models
+﻿using System;
+using System.IO;
+
+namespace ShareLibrary.Models
 {
     public enum Action
     {
@@ -12,5 +15,28 @@
         public FileInfo File;
         public string GroupName;
         public byte[] Data;
+
+        /// <summary>
+        /// Apply the revision the local file.
+        /// </summary>
+        /// <param name="rootFolderPath">Path to the folder of all groups folders.</param>
+        public void Apply(string rootFolderPath)
+        {
+            string filePath = Path.Combine(rootFolderPath, GroupName, File.Name);
+            switch (Action)
+            {
+                case Action.Create:
+                case Action.Modify:
+                    System.IO.File.WriteAllBytes(filePath, Data);
+                    System.IO.File.SetCreationTime(filePath, File.CreationDate);
+                    System.IO.File.SetLastWriteTime(filePath, File.LastModificationDate);
+                    break;
+                case Action.Delete:
+                    System.IO.File.Delete(filePath);
+                    break;
+                default:
+                    throw new ArgumentException("Action non supportée.");
+            }
+        }
     }
 }
